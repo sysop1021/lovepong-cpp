00514750affde1009476eb74ba87aa2349aeaedf
sysop1021 <mattwbrown@gmail.com>
2020-07-27 21:20:33 -0500

    pong-1: without the push stuff, just add Esc key check

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ int main()
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed || (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
                 window.close();
         }
 
