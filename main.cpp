17cd0389cf7e5ec34c586353918b54e4c0cb0516
sysop1021 <mattwbrown@gmail.com>
2020-07-27 20:56:55 -0500

    getting text to screen

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include <SFML/Graphics.hpp>
 
+const int WINDOW_WIDTH = 1280;
+const int WINDOW_HEIGHT = 720;
+const int TEXT_SIZE = 20;
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "pong-0");
+
+    sf::Font font;
+    font.loadFromFile("font.ttf");
+
+    sf::Text text("\"The sky was the color of television, tuned to a dead channel.\"", font);
+    text.setCharacterSize(TEXT_SIZE);
+    sf::FloatRect textBox = text.getGlobalBounds();
+
+    text.setPosition((WINDOW_WIDTH / 2 - (textBox.width / 2)), (WINDOW_HEIGHT / 2 - (TEXT_SIZE / 2)));
 
     while (window.isOpen())
     {
@@ -16,9 +27,8 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(text);
         window.display();
     }
-
     return 0;
 }
